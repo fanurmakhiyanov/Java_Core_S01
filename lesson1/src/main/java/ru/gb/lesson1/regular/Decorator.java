111a79ecb8a5f9b87ec52a448604a306293caa49
fanur <makhiyanov.ff@gmail.com>
2023-05-19 16:12:25 +0300

    Initial commit

---
+++ lesson1/src/main/java/ru/gb/lesson1/regular/Decorator.java
@@ -0,0 +1,17 @@
+package ru.gb.lesson1.regular;
+
+/**
+ * Декаратор. Он декорирурет, то есть, накладывает на результат деокрации.
+ * Внешний вид важен, поэтому этот класс существет.
+ */
+public class Decorator {
+
+    public static String decorate(int a) {
+        /*
+        *  Метод докорирует число, добавляя к нему строку
+        *  при помощи функции форматирования строк
+        */
+        return String.format("Here is your number: %d", a);
+    }
+
+}
--- lesson1/src/main/java/ru/gb/lesson1/sample/Main.java
+++ lesson1/src/main/java/ru/gb/lesson1/sample/Main.java
@@ -1,7 +1,20 @@
 package ru.gb.lesson1.sample;
 
+import ru.gb.lesson1.regular.Decorator;
+import ru.gb.lesson1.regular.OtherClass;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int result = OtherClass.add(2, 3);
+        System.out.println(Decorator.decorate(result));
+
+        result = OtherClass.div(2, 2);
+        System.out.println(Decorator.decorate(result));
+
+        result = OtherClass.mul(2, 2);
+        System.out.println(Decorator.decorate(result));
+
+        result = OtherClass.sub(2, 2);
+        System.out.println(Decorator.decorate(result));
     }
 }
