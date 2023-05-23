b80c70af2530a9cdcbf55b16dfb54ea2f21867e4
fanur <makhiyanov.ff@gmail.com>
2023-05-23 21:40:37 +0300

    finally

--- lesson1/src/main/java/ru/geekbrains/lesson1/sample/Main.java
+++ lesson1/src/main/java/ru/geekbrains/lesson1/sample/Main.java
@@ -7,6 +7,32 @@ import ru.geekbrains.lesson1.regular.OtherClass;
  * javac -sourcepath ./java -d out java/ru/geekbrains/lesson1/sample/Main.java
  *
  * java -classpath ./out ru.geekbrains.lesson1.sample.Main
+ *
+ DOCKERFILE
+ FROM bellsoft/liberica-openjdk-alpine:11.0.16
+ COPY ./java ./src
+ RUN mkdir ./out
+ RUN javac -sourcepath ./src -d out src/ru/geekbrains/lesson1/sample/Main.java
+ CMD java -classpath ./out ru.geekbrains.lesson1.sample.Main
+
+ DOCKERFILE edited
+ FROM bellsoft/liberica-openjdk-alpine:11.0.16 as BuildProject
+ WORKDIR /app
+ COPY ./java ./src
+ RUN mkdir ./out
+ RUN javac -sourcepath ./src -d out ./src/ru/geekbrains/lesson1/sample/Main.java
+
+ FROM scratch as OutputFiles
+ COPY --from=BuildProject /app/out /bin
+
+ docker buildx build --output type=local,dest=. .
+
+ DOCKERFILE v3
+
+ FROM bellsoft/liberica-openjdk-alpine:11.0.16
+ WORKDIR /app
+ COPY ./bin .
+ CMD java -classpath . ru.geekbrains.lesson1.sample.Main
  */
 
 public class Main {
